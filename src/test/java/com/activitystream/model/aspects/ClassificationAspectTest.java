b6908d31e879f1e74dc34a911babdae0f06c4b17
Stefán Baxter <stebax@gmail.com>
2017-09-13 14:38:55 +0000

    Fix the classification test

--- src/test/java/com/activitystream/model/aspects/ClassificationAspectTest.java
+++ src/test/java/com/activitystream/model/aspects/ClassificationAspectTest.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static com.activitystream.model.aspects.AddressAspect.address;
 import static com.activitystream.model.aspects.ClassificationAspect.classification;
 
 
@@ -19,12 +18,11 @@ public class ClassificationAspectTest {
 
         ASEntity venue = new ASEntity("Venue", "983983");
         venue.addAspect(classification()
-                .addType("Concert Hall")
+                .addType("Theater")
+                .addVariant("Concert Hall")
                 .addCategories("Classical","Pop","Variety","Shows"));
 
-        logger.warn("classification: " + venue.toJSON());
-
-        Assert.assertEquals(venue.toJSON().equals("{\"entity_ref\":\"Venue/983983\",\"aspects\":{\"presentation\":{\"label\":\"Royal Albert Hall\",\"description\":\"The Royal Albert Hall is a concert hall on the northern edge of South Kensington, London, which holds the Proms concerts annually each summer since 1941. It has a capacity of up to 5,272 seats.\",\"details_url\":\"https://www.royalalberthall.com/\",\"thumbnail\":\"https://cdn.royalalberthall.com/file/1396975600/32830992449\",\"icon\":\"ol-venue\"}}}"),true);
+        Assert.assertEquals(venue.toJSON().equals("{\"entity_ref\":\"Venue/983983\",\"aspects\":{\"classification\":{\"type\":\"Theater\",\"variant\":\"Concert Hall\",\"categories\":[\"Classical\",\"Pop\",\"Variety\",\"Shows\"]}}}"),true);
     }
 
 }
