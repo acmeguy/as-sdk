14170a7be32ad183a7f774dcd41e37024748633b
Stefán Baxter <stebax@gmail.com>
2017-10-06 06:34:28 +0000

    Schema improvements

--- src/main/java/com/activitystream/model/config/ASConfig.java
+++ src/main/java/com/activitystream/model/config/ASConfig.java
@@ -9,17 +9,35 @@ import java.util.TimeZone;
  */
 public class ASConfig {
 
+    private static enum ServiceLevel {
+        DEVELOPMENT,
+        POC,
+        STAGING,
+        PRODUCTION
+    };
+
+    private static String tenantLabel;
+    private static String apiKey;
+    private static ServiceLevel environment = ServiceLevel.DEVELOPMENT;
+
     private static String defaultCountryCode;
     private static String defaultCurrency;
     private static TimeZone defaultTimeZone;
 
+    public static void credentials(String tenantLabel, String apiKey, ServiceLevel environment) {
+
+        if (apiKey != null) ASConfig.apiKey = apiKey;
+        if (tenantLabel != null) ASConfig.tenantLabel = tenantLabel;
+        if (environment != null) ASConfig.environment = environment;
+
+    }
+
     public static void setDefaults(String countryCode, String currency, TimeZone timeZone) {
-        if (currency != null) {
-            ASConfig.defaultCurrency = currency;
-        }
-        if (countryCode != null) {
-            ASConfig.defaultCountryCode = countryCode;
-        }
+
+        if (currency != null) ASConfig.defaultCurrency = currency;
+
+        if (countryCode != null) ASConfig.defaultCountryCode = countryCode;
+
         if (timeZone != null) {
             JacksonMapper.getMapper().setTimeZone(timeZone);
             DateTimeZone.setDefault(DateTimeZone.forTimeZone(timeZone));
@@ -31,11 +49,16 @@ public class ASConfig {
         return defaultCountryCode;
     }
 
-    public static String getDefaultCurrency() {
-        return defaultCurrency;
-    }
+    public static String getDefaultCurrency() { return defaultCurrency; }
 
     public static TimeZone getDefaultTimeZone() {
         return defaultTimeZone;
     }
+
+    public static String getTenantLabel() { return tenantLabel; }
+
+    public static String getApiKey() { return apiKey; }
+
+    public static ServiceLevel getEnvironment() { return environment; }
+
 }
